98176a30e93eedc93ca25d8490b139b491ba0908
natepowers2009 <95651912+natepowers2009@users.noreply.github.com>
2022-12-02 22:47:36 -0500

    Create Better-Block-Assortments.js
    unfinished

---
+++ Better-Block-Assortments.js
@@ -0,0 +1,33 @@
+(function(ext) {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var descriptor = {
+        blocks: [
+            [' ', 'Previous Costume', 'Prev_costume'],
+            ['r', 'Clone Count', 'clone_count']
+        ]
+    };
+
+    ScratchExtensions.register('Zeg Tools', descriptor, ext);
+})({});
