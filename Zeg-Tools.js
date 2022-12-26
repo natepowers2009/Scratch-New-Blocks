da4e9cdadb608c02d000ef627144235b8168787c
Zeglol1234 <nathanpowers2009@gmail.com>
2022-12-25 22:39:58 -0500

    Update and rename Better-Block-Assortments.js to Zeg-Tools.js

--- Zeg-Tools.js
+++ Zeg-Tools.js
@@ -21,9 +21,9 @@
 
 
 
-
 var descriptor = {
         blocks: [
+            ['b', 'Controller Detected?', 'Ctrllr_Detct'
             [' ', 'Previous Costume', 'Prev_costume'],
             ['r', 'Clone Count', 'clone_count']
         ]
